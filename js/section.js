6c9ff9f2f987b12ac579132864c64758b29df44e
usernameharshchauhan <harshchauhan0994@gmail.com>
2021-01-04 12:46:29 +0530

    added table, some js for rotating subjects, side navBar(using unicode emoji's)

---
+++ js/section.js
@@ -0,0 +1,39 @@
+$(function() {
+
+    const sub = $('.subject');
+    setInterval(function() {
+            var i = Math.floor(Math.random() * 49);
+            console.log(i + " " + sub[i]);
+
+            if (sub[i].style.transform == "rotateZ(-180deg)") {
+                $(sub[i]).addClass('rotatefull');
+            } else {
+                $(sub[i]).addClass('rotatehalf');
+            }
+
+            setTimeout(() => {
+                if ($(sub[i]).hasClass('rotatehalf') == true) {
+                    $(sub[i]).removeClass('rotatehalf');
+                    sub[i].style.transform = "rotateZ(-180deg)";
+                } else {
+                    $(sub[i]).removeClass('rotatefull');
+                    sub[i].style.transform = "rotateZ(0deg)";
+                }
+
+            }, 2000);
+
+        },
+        2000);
+
+
+    $('a.sinnbold').on("mouseover", function() {
+        var x = this;
+        console.log(x);
+        $(x).addClass('slideRight');
+    });
+    $('a.sinnbold').on("mouseout", function() {
+        var x = this;
+        console.log(x);
+        $(x).removeClass('slideRight');
+    });
+});
